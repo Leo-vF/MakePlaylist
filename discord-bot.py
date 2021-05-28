ecc9ecc14df93a6dd88c60e7d2c97827586dae23
Leo-vF <vonfoerster.leo@gmail.com>
2021-05-28 18:49:54 +0200

    added multiple user support

--- discord-bot.py
+++ discord-bot.py
@@ -1,6 +1,6 @@
 import discord
 from os import getenv
-from discord import player
+from discord import user
 from dotenv import load_dotenv
 from main import setup
 from Playlist import from_dcbot, get_avg_analysis
@@ -8,7 +8,8 @@ from Playlist import from_dcbot, get_avg_analysis
 client = discord.Client()
 load_dotenv()
 token = getenv("DISCORD_BOT_TOKEN")
-correct_channel_id = int(getenv("MY_CHANNEL_ID"))
+correct_user = int(getenv("MY_USER_ID"))
+other_users = getenv("USERS").split(",")
 
 
 @client.event
@@ -19,12 +20,18 @@ async def on_message(message: discord.message):
     # print(message.channel)
     # print(message.channel.id)
 
-    if message.channel.id == correct_channel_id:
-        response = handle_commands(message.content)
+    print(message.author.id)
+
+    if message.author.id == correct_user:
+        response = handle_commands_full(message.content)
+        await message.channel.send(response)
+    elif str(message.author.id) in other_users:
+        print(message.author)
+        response = handle_commands(message.content, message.author)
         await message.channel.send(response)
 
 
-def handle_commands(message: str) -> str:
+def handle_commands_full(message: str) -> str:
     parts = message.splitlines()
     _idx = parts[0].find(" ")
     cmd = parts[0][:_idx].strip()
@@ -39,6 +46,26 @@ def handle_commands(message: str) -> str:
         return str(get_avg_analysis(sp, username, playlist))
     else:
         return "Error the commmand you specified does not exist."
+
+
+def handle_commands(message: str, playlist: str) -> str:
+    playlist = str(playlist)
+
+    parts = message.splitlines()
+    cmd = parts[0]
+    songs = parts[1:]
+
+    username, sp = setup()
+
+    if cmd == "add":
+        print(playlist, songs)
+        return from_dcbot(sp, username, str(playlist), songs)
+    elif cmd == "features":
+        return str(get_avg_analysis(sp, username, playlist))
+    elif cmd == "songs":
+        return str()
+    else:
+        return "Error the command you specified does not exist."
 
 
 client.run(token)
